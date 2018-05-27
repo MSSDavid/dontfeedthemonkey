f20a7d7fccb6430c69c0ca13cc193619f9c53cb5
Anderson Luiz <39058196+ADNAnderson@users.noreply.github.com>
2018-05-27 11:54:43 -0300

    Arquivos de Inicialização
    Estou enviando a primeira parte do jogo, onde o jogador clicará num botão para iniciá-lo.

---
+++ Inicial/src/inicial/Inicial.java
@@ -0,0 +1,19 @@
+
+package inicial;
+
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Anderson
+ */
+public class Inicial {
+
+    
+    public static void main(String[] args) {
+        NewJFrame x = new NewJFrame();
+        x.setVisible(true);
+    }
+    
+}
