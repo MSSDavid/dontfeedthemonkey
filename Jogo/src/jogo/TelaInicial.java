7e01908171220c684a420aaf7fe2f65d56f781bd
Anderson Luiz <39058196+ADNAnderson@users.noreply.github.com>
2018-06-10 17:26:45 -0300

    Nova Atualização
    Coloquei um background e alguns inimigos

--- Jogo/src/jogo/TelaInicial.java
+++ Jogo/src/jogo/TelaInicial.java
@@ -75,6 +75,8 @@ public class TelaInicial extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        //Inicia o Jogo
         new Jogo();
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
